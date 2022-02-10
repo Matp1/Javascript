047d4f1cf62514034aae84ce976388cfcf08db9f
Matheus <matheus.paivaads@gmail.com>
2022-02-10 14:46:06 -0300

    adicionando arquivos de ontem e de hoje

--- js-dom-e-seletores/script.js
+++ js-dom-e-seletores/script.js
@@ -1,3 +1,20 @@
+function modificacao(){
+let elementos = document.getElementsByTagName('p')[1].innerText = "Me Vejo bem sucedido na area de desenvolvimento"
+console.log(elementos)
+}
+modificacao()
 
+function cor(){
+    let cores = document.querySelector('.main-content').style.backgroundColor = ('rgb(76,164,109)')
+    console.log(cores)
+    let cores2 = document.querySelector(".center-content").style.backgroundColor = "white"
+    console.log(cores2)
+}
+cor()
 
-      document.getElementById("page-tittle").innerText = "Velozes e Furiosos"
+function correcao(){
+    let corretor = document.getElementsByClassName("title")[0].innerText = "Exercício 5.1 - JavaScript"
+    console.log(corretor)
+}
+correcao()
+
